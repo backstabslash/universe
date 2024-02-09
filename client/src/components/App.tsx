38efcaa7d1624bd32fdd6bf3e23afb0297cf7c28
sherpak <vladislav.rupets@gmail.com>
2024-02-09 17:18:44 +0200

    chakra ui setup

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Button } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Box padding="5" bg="gray.200">
+      <Button colorScheme="red">Test</Button>
+    </Box>
+  );
+};
+
+export default App;
