399d571d23bd07e3ef1a563374b24e1c4b644b81
backstabslash <mr.toppanamor1@gmail.com>
2024-02-10 22:38:58 +0200

    Color pallette

--- client/src/pages/UniversePage.tsx
+++ client/src/pages/UniversePage.tsx
@@ -15,18 +15,24 @@ const contentData = [
   "This is a message",
   "Another message",
   "Yet another message",
+  "This is a message",
+  "Another message",
+  "Yet another message",
+  "This is a message",
+  "Another message",
+  "Yet another message",
 ];
 
 const Sidebar = () => (
   <VStack
     padding="5"
-    bg="#222529"
-    color="#d1d2d3"
+    bg="zinc900"
+    color="zinc300"
     width="300px"
     borderRight="1px"
-    borderColor="#3c3f42"
+    borderColor="zinc600"
   >
-    <Heading mb="5" fontSize="lg">
+    <Heading mb="2" fontSize="lg" width="100%">
       Channels
     </Heading>
     {channels.map((channel) => (
@@ -34,14 +40,14 @@ const Sidebar = () => (
         key={channel}
         p="2"
         borderRadius="md"
-        bg="#222529"
-        color="#d1d2d3"
-        _hover={{ bg: "#2a2d31" }}
+        bg="zinc900"
+        color="zinc300"
+        _hover={{ bg: "zinc800" }}
         width="100%"
       >{`#${channel}`}</Box>
     ))}
     <Divider my="1" />
-    <Heading mt="5" mb="2" fontSize="lg">
+    <Heading mb="2" mt="2" fontSize="lg" width="100%">
       Direct Messages
     </Heading>
     {directMessages.map((user) => (
@@ -49,9 +55,9 @@ const Sidebar = () => (
         key={user}
         p="2"
         borderRadius="md"
-        bg="#222529"
-        color="#d1d2d3"
-        _hover={{ bg: "#2a2d31" }}
+        bg="zinc900"
+        color="zinc300"
+        _hover={{ bg: "zinc800" }}
         width="100%"
       >{`@${user}`}</Box>
     ))}
@@ -62,80 +68,92 @@ const Sidebar = () => (
 const MainContent = () => (
   <Box flex="1" flexDirection="column" h="100vh">
     <Flex
-      bg="#222529"
-      color="#d1d2d3"
+      bg="zinc900"
+      color="zinc300"
       p="4"
       height="60px"
       gap="10px"
       alignItems={"center"}
       borderBottom={"1px"}
-      borderColor={"#3c3f42"}
+      borderColor={"zinc600"}
     >
       <Heading
         flex="1"
         fontSize="2xl"
-        borderColor="#d1d2d3"
-        bg="#2b2f33"
-        padding={"5px"}
-        borderRadius={"md"}
+        borderColor="zinc600"
+        bg="zinc800"
+        padding="5px"
+        borderRadius="md"
         width="200px"
-        textAlign={"center"}
+        textAlign="center"
       >
         Universe
       </Heading>
       <Input
         flex="2"
         placeholder="Search Universe"
-        bg="#2a2d31"
+        bg="zinc800"
         borderRadius="md"
         borderColor="transparent"
-        _focusVisible={{ borderColor: "#3c3f42" }}
-        _hover={{ borderColor: "#3c3f42", bg: "#2b2f33" }}
+        _focusVisible={{ borderColor: "zinc600" }}
+        _hover={{ borderColor: "zinc600", bg: "zinc900" }}
       />
-      <Button bg="#222529" color="#d1d2d3" _hover={{ bg: "#2b2f33" }}>
+      <Button bg="zinc800" color="zinc300" _hover={{ bg: "zinc700" }}>
         ?
       </Button>
     </Flex>
     <Flex>
       <Sidebar />
-      <Box
-        padding="5"
-        bg="#2a2d31"
-        w="100vw"
-        h="calc(100vh - 60px)"
-        color="#d1d2d3"
-      >
-        <Heading mb="5" fontSize="xl">
-          Welcome to #general
-        </Heading>
-        {contentData.map((content, index) => (
-          <Box
-            key={index}
-            p="4"
-            bg="#222529"
-            borderRadius="md"
-            boxShadow="md"
-            mb="4"
-          >
-            {content}
-          </Box>
-        ))}
-        <Flex borderTop="1px" borderColor="#3c3f42" p="4" alignItems="center">
+      <Box bg="zinc900" w="100vw" h="calc(100vh - 60px)" color="zinc300">
+        <Box
+          h="calc(95% - 60px)"
+          overflowY="auto"
+          bgImage="url('https://web.telegram.org/a/chat-bg-pattern-dark.ad38368a9e8140d0ac7d.png')"
+          bgSize="cover"
+          bgRepeat="no-repeat"
+          bgPosition="center"
+        >
+          <Heading p="4" mb="5" fontSize="xl">
+            Welcome to #general
+          </Heading>
+          {contentData.map((content, index) => (
+            <Box
+              key={index}
+              p="3"
+              bg="zinc800"
+              borderRadius="md"
+              boxShadow="md"
+              mb="4"
+              ml="4"
+              width="fit-content"
+            >
+              {content}
+            </Box>
+          ))}
+        </Box>
+        <Flex
+          bg="zinc900"
+          borderTop="1px"
+          borderColor="zinc600"
+          p="4"
+          alignItems="center"
+        >
           <Input
             flex="1"
             placeholder="Type a message..."
-            bg="#2a2d31"
+            bg="zinc800"
             borderRadius="md"
-            borderColor="#3c3f42"
-            _focusVisible={{ borderColor: "#3c3f42" }}
-            _hover={{ borderColor: "#3c3f42", bg: "#2b2f33" }}
+            borderColor="zinc600"
+            _focusVisible={{ borderColor: "zinc600" }}
+            _hover={{ borderColor: "zinc600", bg: "zinc900" }}
             h="60px"
           />
+
           <Button
             ml="4"
-            bg="#2a2d31"
-            color="#d1d2d3"
-            _hover={{ bg: "#222529" }}
+            bg="zinc800"
+            color="zinc300"
+            _hover={{ bg: "zinc700" }}
           >
             Send
           </Button>
