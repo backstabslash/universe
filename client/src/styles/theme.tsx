38b801b64401c9a80623299ffc632c49c7ecfee4
backstabslash <mr.toppanamor1@gmail.com>
2024-02-11 15:25:32 +0200

    Improve theme

---
+++ client/src/styles/theme.tsx
@@ -0,0 +1,24 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    zinc950: '#09090b',
+    zinc900: '#18181b',
+    zinc800: '#27272a',
+    zinc700: '#3f3f46',
+    zinc600: '#52525b',
+    zinc500: '#71717a',
+    zinc400: '#a1a1aa',
+    zinc300: '#d4d4d8',
+    zinc200: '#e4e4e7',
+    zinc100: '#f4f4f5',
+    zinc50: '#fafafa',
+  },
+  fonts: {
+    body: 'Ubuntu, system-ui, sans-serif',
+    heading: 'Ubuntu, system-ui, sans-serif',
+    mono: 'Menlo, monospace',
+  },
+})
+
+export default theme
