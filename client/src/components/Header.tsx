53589699b8b213a32698ad86e45698069f38b7b2
sherpak <vladislav.rupets@gmail.com>
2024-02-17 15:09:59 +0200

    setup modal search

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,39 +1,79 @@
-import { QuestionOutlineIcon } from '@chakra-ui/icons'
-import { Flex, Input, Button } from '@chakra-ui/react'
+import { QuestionOutlineIcon, SearchIcon } from '@chakra-ui/icons'
+import {
+  Flex,
+  Input,
+  Button,
+  useDisclosure,
+  Text,
+  HStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from '@chakra-ui/react'
 
-const Header = (): JSX.Element => (
-  <Flex
-    color="zinc300"
-    p="1"
-    mt="1"
-    height="30px"
-    gap="10px"
-    width="40vw"
-    borderTopLeftRadius={'10px'}
-    alignItems={'center'}
-    justifyContent={'center'}
-  >
-    <Input
-      flex="2"
-      placeholder="Search Universe"
-      fontSize={'sm'}
-      background="rgba(0, 0, 0, 0.5)"
-      borderColor="transparent"
-      height="30px"
-      _placeholder={{ color: 'zinc300' }}
-      _focusVisible={{ borderColor: '' }}
-      _hover={{ borderColor: '', background: 'rgba(0, 0, 0, 0.3)' }}
-    />
-    <Button
-      size={'sm'}
-      bg="transparent"
+const Header = (): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Flex
       color="zinc300"
-      _hover={{ background: 'rgba(0, 0, 0, 0.5)' }}
-      _active={{ background: 'rgba(0, 0, 0, 0.5)' }}
+      p="1"
+      mt="1"
+      height="30px"
+      gap="10px"
+      width="40vw"
+      borderTopLeftRadius={'10px'}
+      alignItems={'center'}
+      justifyContent={'center'}
     >
-      <QuestionOutlineIcon boxSize={'5'} />
-    </Button>
-  </Flex>
-)
+      <Button
+        flex="2"
+        fontSize={'sm'}
+        background="rgba(0, 0, 0, 0.5)"
+        borderColor="transparent"
+        height="30px"
+        _placeholder={{ color: 'zinc300' }}
+        _focusVisible={{ borderColor: '' }}
+        _hover={{ borderColor: '', background: 'rgba(0, 0, 0, 0.3)' }}
+        onClick={onOpen}
+      >
+        <HStack justifyContent="space-between" width="100%" color="zinc300">
+          <Text>Search Universe</Text>
+          <SearchIcon />
+        </HStack>
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} size="2xl">
+        <ModalOverlay bg="transparent" />
+        <ModalContent mt={1} bg="#18181b">
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Search Universe</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Button
+        alignSelf=""
+        size={'sm'}
+        bg="transparent"
+        color="zinc300"
+        _hover={{ background: 'rgba(0, 0, 0, 0.5)' }}
+        _active={{ background: 'rgba(0, 0, 0, 0.5)' }}
+      >
+        <QuestionOutlineIcon boxSize={'5'} />
+      </Button>
+    </Flex>
+  )
+}
 
 export default Header
