7291935f1bb95439f88671ac06b6153b2c156e81
Vladislav Rupets <84872808+vladislavrupets@users.noreply.github.com>
2024-05-31 00:08:46 +0300

    Fix profile (#86)

--- server/src/models/channel/channelModel.ts
+++ server/src/models/channel/channelModel.ts
@@ -1,6 +1,5 @@
 import { Schema, model, Document } from "mongoose";
 import { IUser } from "../user/userModel";
-import { boolean } from "joi";
 
 export interface IChannel extends Document {
   name: string;
